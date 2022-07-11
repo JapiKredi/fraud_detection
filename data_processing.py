522c02cb954f96cfbe7472b09139ac8e0bcbbca4
JapiKredi <jasper.bongers@yahoo.com>
2022-07-11 18:30:56 +0200

    Have added a new function sum

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,13 @@
+from calendar import c
+
+
 print("Adding all the code for data processing related to Fraud detection")
+
+a = 5
+b = 7
+
+def sum(a,b):
+    c = a+b
+    return a
+
+print("added new function sum which is not correct")
