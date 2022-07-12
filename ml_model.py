f814d837747298438d4c1d59b51db78def2a56ab
JapiKredi <jasper.bongers@yahoo.com>
2022-07-12 13:55:34 +0200

    added model1, model2, model3 to the ml_models file

--- ml_model.py
+++ ml_model.py
@@ -1,2 +1,15 @@
 
 print("adding all ML related code here")
+
+def model1():
+    print("This is the code for model1")
+
+
+def model2():
+    print("This is the code for model2")
+
+
+def model3()):
+    print("This is the code for model3")
+
+
